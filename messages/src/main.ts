f0204c1aa44a6ab0e57ef431f599635ba28c72dc
Shubham0599 <b18shubham@gmail.com>
2024-01-16 23:26:24 +0530

    check

--- messages/src/main.ts
+++ messages/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { MessageModule } from './message/message.module';
 import { ValidationPipe } from '@nestjs/common';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(MessageModule);
   app.useGlobalPipes(new ValidationPipe())
